9d7e36b4ad04b67b71681d108776798452ed1c1d
Jacob Ogle <ogle.jacobd@gmail.com>
2025-07-30 10:36:04 -0400

    Neetcode - AH - ContainsDup

--- NeetcodeRoadmap/ArraysAndHashing/contains_duplicate.py
+++ NeetcodeRoadmap/ArraysAndHashing/contains_duplicate.py
@@ -6,4 +6,25 @@ from typing import List
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        return True
+        return self.sets_approach(nums)
+
+
+    def hashmap_approach(self, nums: List[int]) -> bool:
+        # TC: O(n)
+        # SC: O(n) -- map creation set() uses this under the hood
+        m = {}
+
+        for num in nums:
+            if num in m:
+                return True
+            else:
+                m[num] = 0
+        return False
+
+
+    def sets_approach(self, nums: List[int]) -> bool:
+        # TC: O(n)
+        # SC: O(n) - new set creation
+        nums_set = set(nums)
+        return len(nums_set) != len(nums)
+        
