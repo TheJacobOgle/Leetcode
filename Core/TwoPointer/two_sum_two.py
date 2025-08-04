d1557d0b78aaec96239df7a8bdc54ab745c25e8e
Jacob <ogle.jacobd@gmail.com>
2025-08-04 10:56:52 -0400

    TwoPointers

---
+++ Core/TwoPointer/two_sum_two.py
@@ -0,0 +1,6 @@
+# Leetcode 167
+# https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/description/
+# Medium
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        pass
