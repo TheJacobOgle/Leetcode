17365c57485c8b07f1692ddae54bed3ed35b0246
Jacob Ogle <ogle.jacobd@gmail.com>
2025-08-06 12:16:43 -0400

    dfs

---
+++ Core/ArraysAndHasing/contains_duplicate.py
@@ -0,0 +1,31 @@
+# LC Problem 217
+# Easy
+# Topic: Arrays and Hashing
+# Link: https://leetcode.com/problems/contains-duplicate/description/
+
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return self.sets_approach(nums)
+
+
+    def hashmap_approach(self, nums: List[int]) -> bool:
+        # TC: O(n)
+        # SC: O(n) -- map creation set() uses this under the hood
+        m = {}
+
+        for num in nums:
+            if num in m:
+                return True
+            else:
+                m[num] = 0
+        return False
+
+
+    def sets_approach(self, nums: List[int]) -> bool:
+        # TC: O(n)
+        # SC: O(n) - new set creation
+        nums_set = set(nums)
+        return len(nums_set) != len(nums)
+        
---
+++ Core/TwoPointer/container_with_most_water.py
@@ -0,0 +1,6 @@
+# Leetcode 11
+# https://leetcode.com/problems/container-with-most-water/description/
+# Medium
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        pass
